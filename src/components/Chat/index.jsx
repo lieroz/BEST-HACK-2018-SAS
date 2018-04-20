97b8711169e7a8b3d226204fce2f40278dc85d96
Andrey Kamakin <a.kamakin@icloud.com>
2018-04-21 01:58:46 +0300

    Added comunication with shit server

--- src/components/Chat/index.jsx
+++ src/components/Chat/index.jsx
@@ -2,5 +2,6 @@
  * Created by ed on 21/04/2018.
  */
 
-'use strict';
+import React from 'react';
 
+class Chat extends React.Component {}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
-import { Router, Route, Switch } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createBrowserHistory } from 'history';
+import { Router, Route, Switch } from 'react-router-dom';
 
-import "assets/css/material-dashboard-react.css?v=1.2.0";
+import 'assets/css/material-dashboard-react.css?v=1.2.0';
 
-import indexRoutes from "routes/index.jsx";
+import indexRoutes from 'routes/index.jsx';
 
 import veryCleverModule from './veryCleverModule';
+import ServerApi from './ServerApi/ServerApi';
 
 const hist = createBrowserHistory();
+
+let serverApi = new ServerApi('http://localhost:8000');
+serverApi.getData().then(res => {
+  let api = new veryCleverModule();
+  Object.keys(res.data).forEach(intent => {});
+});
 
 ReactDOM.render(
   <Router history={hist}>
@@ -19,5 +26,5 @@ ReactDOM.render(
       })}
     </Switch>
   </Router>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
