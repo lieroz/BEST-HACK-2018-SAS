fc91edbff053ead6e39a6e12f8546dda739fc5c5
Andrey Kamakin <a.kamakin@icloud.com>
2018-04-21 04:13:44 +0300

    Added services

--- src/views/Services/Services.jsx
+++ src/views/Services/Services.jsx
@@ -15,6 +15,9 @@ import {
   ItemGrid
 } from 'components';
 
+import Toggle from 'react-toggle';
+import './temp.css';
+
 const style = {
   typo: {
     paddingLeft: '25%',
@@ -35,31 +38,41 @@ const style = {
     width: '260px'
   }
 };
+
+const services = [];
+
+function fillServices() {
+  for (let i = 0; i < 100; i++) {
+    services.push({ name: 'Услуга ' + i.toString() });
+  }
+}
+
+fillServices();
+
+console.info(services);
+
 function ServicesPage({ ...props }) {
   return (
     <div>
-      <Grid container>
-        <ItemGrid xs={12} sm={12} md={6}>
-          <RegularCard
-            cardTitle={'Подключенные услуги'}
-            cardSubtitle={'Список всех подключенных услуг'}
-            headerColor={'green'}
-            content={
-              <div>
-                <Grid container />
-              </div>
-            }
-          />
-        </ItemGrid>
-        <ItemGrid xs={12} sm={12} md={6}>
-          <RegularCard
-            cardTitle={'Неподключенные услуги'}
-            cardSubtitle={'Список всех неподключенных услуг'}
-            headerColor={'orange'}
-            content={<div />}
-          />
-        </ItemGrid>
-      </Grid>
+      <RegularCard
+        cardTitle={'Подключенные услуги'}
+        cardSubtitle={'Список всех подключенных услуг'}
+        headerColor={'green'}
+        content={
+          <div>
+            <Grid container>
+              {services.map(function(d, idx) {
+                return (
+                  <ItemGrid xs={12} sm={12} md={4} key={idx}>
+                    <Toggle defaultChecked={false} onChange={false} />
+                    <span style={{ padding: 10 }}>{d.name}</span>
+                  </ItemGrid>
+                );
+              })}
+            </Grid>
+          </div>
+        }
+      />
     </div>
   );
 }
