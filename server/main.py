94e8e427ac119909bc8806da077f4f9249ab896c
Andrey Kamakin <a.kamakin@icloud.com>
2018-04-21 05:13:33 +0300

    Lesha poprosil zapushit

--- server/main.py
+++ server/main.py
@@ -3,8 +3,7 @@ from flask_simplelogin import SimpleLogin, login_required
 from flask_cors import CORS
 
 import json
-dict={}
-
+fname='dict.txt'
 
 app = Flask(__name__, static_url_path='/static',  static_folder='static')
 app.config['SECRET_KEY'] = 'something-secret'
@@ -12,19 +11,21 @@ app.config['SIMPLELOGIN_USERNAME'] = 'chuck'
 app.config['SIMPLELOGIN_PASSWORD'] = 'norris'
 SimpleLogin(app)
 CORS(app)
+
 @app.route('/', methods=['GET'], defaults={'path': ''})
 def main_p(path):
     page = render_template('index.html')
     res = make_response(page, 200)
     return res
 
-#небезапасно!
 @app.route('/formdata')
 def formData():
     intentName = request.args.get('intentName')
     url = request.args.get('url')
     if(intentName!='') and (url != ''):
-        dict[intentName] = url
+        f = open( fname, 'a' )
+        f.write(intentName, " ", url )
+        f.close()
         return "Your data have saved!"
     else:
         return "Your data is incorrect!"
@@ -33,7 +34,19 @@ def formData():
 
 @app.route('/getdata')
 def getData():
+    f = open( fname, 'a' )
+    line = f.readline()
+    dict={}
+    while line:
+        values = line.split(" ")
+        dict[values[0]] = values[1]
+        line = f.readline()
     return json.dumps(dict, ensure_ascii=False)
+
+@app.route('/cleardata')
+def clearData():
+    open(fname, 'w').close()
+    return 0
 
 
 @app.route('/admin')
@@ -46,4 +59,4 @@ def foo():
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', 8000, threaded=True, debug=True)
+    app.run('0.0.0.0', 8000, debug=True)
